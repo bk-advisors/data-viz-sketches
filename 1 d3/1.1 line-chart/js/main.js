8e5968301080affb69c6d7d21c81b2969cbcbb39
mattykuch <bk.advisors.com@gmail.com>
2025-09-18 21:14:02 +0300

    Added comments on 1 access data

--- 1 d3/1.1 line-chart/js/main.js
+++ 1 d3/1.1 line-chart/js/main.js
@@ -1,23 +1,28 @@
 async function drawLineChart() {
   // 1. Access the data
 
-  // async because we might fetch data from an external source
+  // We build our code inside a function because we need to think of this like starting a line-chart recipe, we can replicate again and again
+
+  // It’s "async" because sometimes we have to wait for ingredients (data) to arrive from the store (the CSV file).
 
   // 1.1 Load the data
 
   const dataset2 = await d3.csv(
+    // await means: “Don’t start cooking yet, wait until the groceries (data) actually arrive.”
     "./data/nyc_weekly_temperature.csv",
-    d3.autoType // d3.autoType method automatically changes the max-temp & date to JS format
+    d3.autoType // d3.autoType is like a helper who automatically converts the groceries into the right type: numbers, dates, etc.
   );
 
   // 1.2 Create Accessor for y and x data points
 
-  yAccessor = (d) => d.max_temp_F;
-  xAccessor = (d) => d.date;
+  // An accessor is like a special tool that helps us pick out the exact ingredients (data points) we need from larger the dataset.
 
-  console.log(yAccessor(dataset2[0])); // confirm by console loging
+  yAccessor = (d) => d.max_temp_F; // d is each row of the dataset, we want to access the max_temp_F column
+  xAccessor = (d) => d.date; // d is each row of the dataset, we want to access the date column
 
-  console.log(xAccessor(dataset2[0])); // confirm by console loging
+  //console.log(yAccessor(dataset2[0])); // Test the accessor by logging the first data point's max_temp_F value
+
+  //console.log(xAccessor(dataset2[0])); // Test the accessor by logging the first data point's date value
 }
 
 drawLineChart();
