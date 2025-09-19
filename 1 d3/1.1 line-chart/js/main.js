ed3b665cde3d55865d01979f20ff2cc8373b2237
mattykuch <bk.advisors.com@gmail.com>
2025-09-19 12:04:50 +0300

    update main.js 20250919

--- 1 d3/1.1 line-chart/js/main.js
+++ 1 d3/1.1 line-chart/js/main.js
@@ -23,6 +23,47 @@ async function drawLineChart() {
   //console.log(yAccessor(dataset2[0])); // Test the accessor by logging the first data point's max_temp_F value
 
   //console.log(xAccessor(dataset2[0])); // Test the accessor by logging the first data point's date value
+
+  // 2. Create chart dimensions
+  // 2.1 Store the dimensions that wraps around the 1st layer "wrapper" of the chart (width, height and margins) in an array
+
+  let dimensions = {
+    width: window.innerWidth * 0.9, // Use the innerwidth of the screen-window to determine the width
+    height: 400,
+    margin: {
+      // The margin between the wrapper layer and the inner boundary i.e. 2nd inner layer
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  };
+
+  // 2.2 Calculate the bounded width and height of the inner layer (where the data is drawn)
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.right - dimensions.margin.left;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // 3. Draw the canvas
+  // 3.1 Select the chart container in the HTML and append the SVG canvas element
+
+  const wrapper = d3
+    .select("#wrapper") // Select the div with id "wrapper" in the HTML
+    .append("svg") // Append an SVG element to the wrapper div
+    .attr("width", dimensions.width) // Set the width of the SVG element
+    .attr("height", dimensions.height); // Set the height of the SVG element
+
+  // 3.2 Append a "g" element to the SVG and move it to the top left margin
+
+  const bounds = wrapper
+    .append("g") // Append a "g" element to the SVG, this "g" will contain all the data elements
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)` // Move the "g" element to the top left margin
+    );
 }
 
 drawLineChart();
