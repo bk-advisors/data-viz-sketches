fdddb9ee78944bfeffc27d6391e0b99288687ad8
mattykuch <bk.advisors.com@gmail.com>
2025-09-21 13:32:50 +0300

    update main.js 20250921

--- 1 d3/1.1 line-chart/js/main.js
+++ 1 d3/1.1 line-chart/js/main.js
@@ -48,7 +48,7 @@ async function drawLineChart() {
     dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw the canvas
-  // 3.1 Select the chart container in the HTML and append the SVG canvas element
+  // 3.1 Select the wrapper container in the HTML and append the SVG canvas element
 
   const wrapper = d3
     .select("#wrapper") // Select the div with id "wrapper" in the HTML
@@ -64,6 +64,81 @@ async function drawLineChart() {
       "transform",
       `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)` // Move the "g" element to the top left margin
     );
+
+  // 4. Create scales
+  // 4.1 Create the y scale (temperature scale)
+
+  const yScale = d3
+    .scaleLinear() // Create a linear scale for the y axis
+    .domain(d3.extent(dataset2, yAccessor)) // Set the domain of the y scale to the extent (min and max) of the max_temp_F values in the dataset
+    .range([dimensions.boundedHeight, 0]); // Set the range of the y scale to be from the bottom to the top of the bounded height
+  // Note: In SVG, the y=0 coordinate is at the top, so we reverse the range
+
+  // console.log(d3.extent(dataset2, yAccessor)); // confirm by console logging it
+
+  // 4.2 Create the x scale (time scale)
+  const xScale = d3
+    .scaleTime() // Create a time scale for the x axis
+    .domain(d3.extent(dataset2, xAccessor)) // Set the domain of the x scale to the extent (min and max) of the date values in the dataset
+    .range([0, dimensions.boundedWidth]); // Set the range of the x scale to be from left to right of the bounded width
+
+  // console.log(d3.extent(dataset2, xAccessor)); // confirm by console logging it
+
+  // 5. Draw data
+
+  // 5.1 Create a line generator function
+
+  const lineGenerator = d3
+    .line() // Create a line generator
+    .x((d) => xScale(xAccessor(d))) // Set the x value of each point in the line using the x scale and x accessor
+    .y((d) => yScale(yAccessor(d))) // Set the y value of each point in the line using the y scale and y accessor
+    .curve(d3.curveBasis); // Apply a curve to the line for smoothness  // Other curve options: d3.curveLinear, d3.curveStep, d3.curveCardinal, etc.
+
+  // 5.2 Use the line generator to create the "d" attribute for a "path" element
+  const line = bounds
+    .append("path") // Append a "path" element to the bounds "g"
+    .attr("d", lineGenerator(dataset2)) // Use the line generator to create the "d" attribute for the path, passing in the dataset
+    .attr("fill", "none") // Set fill to none because we only want a line, not a filled shape
+    .attr("stroke", "#af9358") // Set the stroke color of the line
+    .attr("stroke-width", 2); // Set the stroke width of the line
+
+  // 6. Draw peripherals
+  // 6.1 Create and draw the y axis
+
+  const yAxisGenerator = d3.axisLeft().scale(yScale); // Create a left-oriented axis generator using the y scale
+  const yAxis = bounds.append("g").call(yAxisGenerator); // Append a "g" element to the bounds and call the y axis generator to draw the y axis
+
+  // 6.2 Create and draw the x axis
+
+  const xAxisGenerator = d3.axisBottom().scale(xScale); // Create a bottom-oriented axis generator using the x scale
+  const xAxis = bounds
+    .append("g") // Append a "g" element to the bounds
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`) // Move the x axis to the bottom of the bounded height
+    .call(xAxisGenerator); // Call the x axis generator to draw the x axis
+
+  // 6.3 Add a label to the y axis
+
+  const yAxisLabel = bounds
+    .append("text") // Append a "text" element to the y axis "g"
+    .attr("x", -dimensions.boundedHeight / 2) // Position the label in the middle of the y axis
+    .attr("y", -dimensions.margin.left + 15) // Position the label to the left of the y axis
+    .html("Max Temperature (ºF)") // Set the text of the label
+    .style("font-size", "1.4em") // Set the font size of the label
+    .style("text-anchor", "middle") // Center the text anchor
+    .style("transform", "rotate(-90deg)"); // Rotate the label to be vertical
+  // 6.4 Add a label to the x axis
+
+  const xAxisLabel = bounds
+    .append("text") // Append a "text" element to the x axis "g"
+    .attr("x", dimensions.boundedWidth / 2) // Position the label in the middle of the x axis
+    .attr("y", dimensions.margin.bottom + 345) // Position the label below the x axis
+    .html("Month") // Set the text of the label
+    .style("font-size", "1.4em") // Set the font size of the label
+    .style("text-anchor", "middle"); // Center the text anchor
+
+  // 7. Set up interactions (if any)
+
+  // No interactions for this simple line chart
 }
 
 drawLineChart();
